cab7aede2eae445742c787d3484ff8e5f18d6015
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2018-07-08 00:12:28 +0300

    add labels

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 var width = 800,
     height = 350,
-    padding = {x: 200, y: 70}
+    padding = {x: 250, y: 70}
     x = d3.scaleBand().range([0, width - padding.x], 0).padding(0.2),
     y = d3.scaleLinear().range([height - padding.y, 0]);
 
@@ -12,10 +12,11 @@ var canvas = d3.select('body').append('svg')
                 .attr('class', 'canvas')
                 .attr('width', width)
                 .attr('height', height)
+                .attr('style', 'border: 1px solid #eee')
                 .append('g')
                 .attr('width', width - padding.x)
                 .attr('height', height - padding.y)
-                .attr('transform', 'translate(50, 0)');
+                .attr('transform', 'translate('+ 50 +', 10)');
 
 d3.json('js/data.json').then((res) => {
 
@@ -30,14 +31,16 @@ d3.json('js/data.json').then((res) => {
         .attr('transform', 'translate(0, '+ (height - padding.y) +')')
         .call(axisX)
         .selectAll('text')
-        .attr("y", 30)
+        .style('text-anchor', 'start')
+        .attr("y", 10)
         .attr('transform', 'rotate(10)');                  
 
     canvas.append('g')
         .attr('class', 'y axis')
-        .call(axisY)        
-        .style('text-anchor', 'end')
-        .append('text')                
+        .call(axisY)
+        .append('text')
+        .attr('transform', 'translate(10, 0) rotate(-90)')
+        .attr('fill', 'black')        
         .text('Pages');
     
     canvas.append('g')
@@ -50,6 +53,6 @@ d3.json('js/data.json').then((res) => {
         .attr('x', (d) => { return x(d.title) })
         .attr('width', x.bandwidth())
         .attr('y', (d) => { return y(d.pages) })
-        .attr('height', (d) => { return height - y(d.pages) })
-        .attr('transform', 'translate(0, -'+ padding.y +')')
+        .attr('height', (d) => { return height - padding.y - y(d.pages) })
+        
 });
